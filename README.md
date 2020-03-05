4a9b9b16c33ef0c0094612baf2998922076ab136
pjaudiomv <pjaudiomv@gmail.com>
2020-03-04 21:57:47 -0500

    update readme
    remove unused dt object
    
    readme update
    
    readme update
    
    formating changes
    
    mo changes

--- README.md
+++ README.md
@@ -2,4 +2,40 @@
 
 Checks for failed tomato imports and then sends slack notification to the BMLT-Enabled `root-status` channel.
 
-a cloudwatch event rule triggers the lambda when a tomato ecs task state changes.
+## What does this do?
+
+This creates a lambda that gets triggered whenever the tomato ECS task state changes, It then checks if the desired 
+status is stopped so we know that the task has ended. We then hit the root server endpoint of tomato and check if any of 
+last successful import times is less than the ECS task start time. If it is then it sends a slack notification to the 
+BMLT-Enabled `root-status` channel.
+
+## Why?
+
+I had previously created a php script that accomplishes this similarly but was never really happy with it. It ran on a cron
+and wasn't precise enough. Adding to 47 bot would of been more of the same. With this it gets triggered as soon as the import
+is done and we will know right away if a import failed.
+
+## Setup
+
+To install clone this repo, then from the terraform directory. Set the `slack_webhook` variable in the `variables.tf` file to the slack incoming web hook. Then run.
+
+```
+terraform init
+```
+
+This will initialize the terraform. now run.
+
+```
+terraform plan
+```
+
+This will zip the lambda using the archive data source, As well as give you the plan for what the apply stage will 
+perform. If all looks good you can now run.
+
+```
+terraform apply
+```
+
+## TODO:
+If a root server fails an import, it would be interesting to look into the possibility of pulling the cloudwatch logs and 
+attaching the error message along with the slack notification. Most of the time they are just connection errors. 
--- terraform/tfin_lambda.py
+++ terraform/tfin_lambda.py
@@ -1,10 +1,11 @@
 import os
 import json
-from datetime import datetime, time
+from datetime import datetime
 from urllib.request import *
 
 tomato_url = "https://tomato.bmltenabled.org/rest/v1/rootservers/"
 slack_url = os.environ.get('SLACK_WEBHOOK')
+
 
 def send_slack_alert(message_to_send, webhook_url):
     payload = {
@@ -15,7 +16,10 @@ def send_slack_alert(message_to_send, webhook_url):
         "attachments": [message_to_send]
     }
 
-    req = Request(url=webhook_url, data=json.dumps(payload).encode(), headers={'Content-Type': 'application/json'}, method='POST')
+    req = Request(url=webhook_url,
+                  data=json.dumps(payload).encode(),
+                  headers={'Content-Type': 'application/json'},
+                  method='POST')
     with urlopen(req) as res:
         body = res.read().decode()
         print(body)
@@ -23,8 +27,6 @@ def send_slack_alert(message_to_send, webhook_url):
 
 def lambda_handler(event, context):
     task_state_detail = event["detail"]
-    if "RUNNING" in task_state_detail["desiredStatus"]:
-        return
     if "STOPPED" in task_state_detail["desiredStatus"]:
         started_at = task_state_detail["startedAt"]
         started_at_dt = datetime.strptime(started_at, '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -39,24 +41,25 @@ def lambda_handler(event, context):
         for i in root_servers:
             last_import_dt = datetime.strptime(i["last_successful_import"], '%Y-%m-%dT%H:%M:%S.%fZ')
             last_import_ts = (last_import_dt - datetime(1970, 1, 1)).total_seconds()
-            lsi_object = datetime.strptime(i["last_successful_import"], "%Y-%m-%dT%H:%M:%S.%f%z")
             last_import_formatted = last_import_dt.strftime("%d-%b-%Y (%H:%M:%S)")
 
             if last_import_ts < started_at_ts:
                 print("Failed Import: ", i["name"])
                 message = {"color": "#ff6600",
-                  "fallback": "Tomato Failed Import.",
-                  "title": "Tomato Failed Import",
-                  "title_link": "https://tomato.na-bmlt.org/rest/v1/rootservers/",
-                  "footer": "BMLT-Enabled",
-                  "footer_icon": "https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2018-12-26/512035188372_266e0f7e633d3b17af73_132.png",
-                  "ts": datetime.utcnow().timestamp(),
-                  "fields": [
-                      {"title": "Root Server", "value": i["root_server_url"], "short": False},
-                      {"title": "Last Import", "value": last_import_formatted, "short": True}
-                  ]
-                  }
+                           "fallback": "Tomato Failed Import.",
+                           "title": "Tomato Failed Import",
+                           "title_link": "https://tomato.na-bmlt.org/rest/v1/rootservers/",
+                           "footer": "BMLT-Enabled",
+                           "footer_icon": "https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2018-12-26/512035188372_266e0f7e633d3b17af73_132.png",
+                           "ts": datetime.utcnow().timestamp(),
+                           "fields": [
+                               {"title": "Root Server", "value": i["root_server_url"], "short": False},
+                               {"title": "Last Import", "value": last_import_formatted, "short": True}
+                           ]
+                           }
                 if slack_url is not None and slack_url != "":
                     send_slack_alert(message, slack_url)
                 else:
                     print(message)
+    else:
+        return
